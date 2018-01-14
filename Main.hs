8a9331a13d044e4fe5eca894e39e1796148bc688
NidzaKornjaca <nikola.vlahovic2401@gmail.com>
2018-01-14 04:42:39 -0800

    Add comments
    Signed-off-by: NidzaKornjaca <nikola.vlahovic2401@gmail.com>

--- Main.hs
+++ Main.hs
@@ -10,46 +10,59 @@ main :: IO ()
 main = do
   _ <- initGUI
   builder <- builderNew
+
+  -- load ui
   builderAddFromFile builder "MainWindow.ui"
   window <- builderGetObject builder castToWindow "mainWindow"
   viewport <- builderGetObject builder castToViewport "fieldsViewport"
   frame <- builderGetObject builder castToContainer "templateAlignment"
   fFilter <- builderGetObject builder castToFileFilter "templateFilter"
-
   generateButton <- builderGetObject builder castToButton "generateButton"
+
+  -- "Load templates" button setup
   templateButton <- fileChooserButtonNew "Select template" FileChooserActionOpen
-
   fileChooserAddFilter (toFileChooser templateButton) fFilter
-
   containerAdd frame templateButton
 
+
+  -- bind callbacks for static ui objects
   on window deleteEvent $ liftIO mainQuit >> return False
+
+  let getTemplateFilePath = (getFieldData $ castToWidget templateButton)
+
   on templateButton fileChooserButtonFileSet
-    $ liftIO $ onTemplateSelected viewport generateButton (getFieldData $ castToWidget templateButton)
+    $ liftIO $ onTemplateSelected viewport generateButton getTemplateFilePath
     >> widgetShowAll window
 
   widgetShowAll window
   mainGUI
 
-
+-- callback function for loading a template file
 onTemplateSelected container generateButton templateFile = do
+  -- clear container
   containerForeach container $ containerRemove container
+  -- create vbox for the dynamic fields
   vbox <- vBoxNew True 10
   containerAdd container vbox
-  template <- templateFile >>= templateLoader . (drop 7)
+  -- load template from file
+  template <- templateFile >>= loadTemplateFromPath . (drop 7)
+  -- generate fields
   fieldsInputs <- createInputs (toBox vbox) template
-  on generateButton buttonReleaseEvent $ tryEvent $ liftIO $ getValuesHandler template fieldsInputs
+  -- rebind generateButton event
+  on generateButton buttonReleaseEvent $ tryEvent $ liftIO $ generateHandler template fieldsInputs
   return ()
 
-getValuesHandler layout fields = do
+
+generateHandler layout fields = do
   e <- getFieldsData fields
   print $ fromFields layout e
 
 
-templateLoader path = do
+loadTemplateFromPath path = do
   defaultTmp <- loadTemplate path
   return $ case (defaultTmp) of
     Right r -> r
-    Left error -> MetaLabel {message = "Can't load file"}
+    Left error -> MetaLabel {message = "Can't load file"}  -- display error
 
-defaultTemplate = templateLoader "template-guide.vkt"
+
+defaultTemplate = loadTemplateFromPath "template-guide.vkt"
--- VKForm.hs
+++ VKForm.hs
@@ -3,6 +3,7 @@ module VKForm where
     import Graphics.UI.Gtk
     import VKTemplate
 
+    -- get fields from a layout object
     getFields :: LayoutObject -> [LayoutObject]
     getFields (RectLayout _ _ _ children) = _getFieldsListHelper children
     getFields obj@(MetaLabel _) = [obj]
@@ -13,6 +14,7 @@ module VKForm where
     _getFieldsListHelper (x:xs) = getFields x ++ _getFieldsListHelper xs
 
 
+    -- transforms a layout object to it's widget representation
     _toInput :: LayoutObject -> IO (Widget, Widget)
     _toInput (TextField l _ _ _) = do
         entr <- entryNew
@@ -26,6 +28,7 @@ module VKForm where
         ml <- labelNew $ Just l
         return (castToWidget ml, castToWidget ml)
 
+    -- takes a widget and puts it into a vbox with a label
     _wrapLabeledElement elem l = do
         vbox <- vBoxNew True 10
         lbl <- labelNew $ Just l
@@ -33,7 +36,7 @@ module VKForm where
         boxPackStart vbox elem PackGrow 0
         return $ castToWidget vbox
 
-
+    -- creates input fields from a LayoutObject template and packs them into the parent Box
     createInputs :: Box -> LayoutObject -> IO [Widget]
     createInputs parent template = do
         entries <- mapM _toInput $ getFields template
@@ -42,10 +45,9 @@ module VKForm where
 
     pack layout item = boxPackStart layout item PackNatural 0
 
-
+    -- extract input values
     getFieldsData :: [Widget] -> IO [String]
     getFieldsData l = mapM getFieldData l
-
 
     getFieldData :: Widget -> IO String
     getFieldData widget
@@ -55,6 +57,7 @@ module VKForm where
             return $ fromMaybe "" s
 
 
+    -- return a non field layout object analogue, with value=value
     fromField :: LayoutObject -> String -> LayoutObject
     fromField (ImageField _ p s) value = Image {
         position = p, size = s, value = value
@@ -63,27 +66,31 @@ module VKForm where
         position = p, size = s, value = value, fontSize = f
     }
 
+    -- functions for counting field layout objects within a layout object
     countFieldsInSubtree (RectLayout _ _ _ l) = countFieldsInL l
     countFieldsInSubtree x
         | isField x = 1
         | otherwise = 0
     countFieldsInL (x:xs) = countFieldsInSubtree x + countFieldsInL xs
 
+    -- for a list of layout objects, returns their non-field layout object analogues
+    -- values for the fromField transofmation are used in order
     flListHelper :: [LayoutObject] -> [String] -> [LayoutObject]
     flListHelper [] _ = []
     flListHelper x [] = x
     flListHelper (x@(RectLayout _ _ _ _):xs) values =
-        (_fromFields x values) : flListHelper xs  (drop (countFieldsInSubtree x) values)
+        (fromFields x values) : flListHelper xs  (drop (countFieldsInSubtree x) values)
     flListHelper (x:xs) values@(v:vs)
         | isField x = fromField x v : flListHelper xs vs
         | otherwise = x : flListHelper xs values
 
-    fromFields :: LayoutObject -> [String] -> LayoutObject
-    fromFields lyt values = _fromFields lyt values
 
-    _fromFields :: LayoutObject -> [String] -> LayoutObject
-    _fromFields (RectLayout n s p l) values = RectLayout {
+    -- for a layout object and list of values, returns a layout object
+    -- with it's non-field object analogues
+    -- the string values are used in order
+    fromFields :: LayoutObject -> [String] -> LayoutObject
+    fromFields x [] = fromField x ""
+    fromFields (RectLayout n s p l) values = RectLayout {
         name = n, size = s, position = p, children = flListHelper l values
     }
-    _fromFields x (v:_) = fromField x v
-    _fromFields x [] = fromField x ""
+    fromFields x (v:_) = fromField x v

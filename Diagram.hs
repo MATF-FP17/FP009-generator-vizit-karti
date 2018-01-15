a5e6cbd5c5b817f05e27c202e040847d1a7e35be
NidzaKornjaca <nikola.vlahovic2401@gmail.com>
2018-01-14 19:35:35 -0800

    Import images
    Signed-off-by: NidzaKornjaca <nikola.vlahovic2401@gmail.com>

--- Diagram.hs
+++ Diagram.hs
@@ -4,64 +4,66 @@
 
 module Diagram where
 
-import Diagrams.Prelude
-import Diagrams.Backend.SVG
-import VKTemplate 
+    import Diagrams.Prelude
+    import Diagrams.Backend.Rasterific
+    import VKTemplate
+    import Data.Either
+    import System.IO.Unsafe
 
-_myDiagramHelper :: [LayoutObject] -> Diagram B
-_myDiagramHelper [x] = myDiagram x
-_myDiagramHelper (x:xs) =   myDiagram x |||
-                            strutX 5 |||
-                            _myDiagramHelper xs
+    _myDiagramHelper :: [LayoutObject] -> Diagram B
+    _myDiagramHelper [x] = myDiagram x
+    _myDiagramHelper (x:xs) =   _myDiagramHelper xs `atop` myDiagram x
 
-myDiagram :: LayoutObject -> Diagram B
-myDiagram (RectLayout _ (Size width height) (Position x y) children)  = 
-    (_myDiagramHelper children) 
-    `atop` square 1
-    #fc white
-    # scaleX width
-    # scaleY height
-    # lwG 0.05
-    # translateX x
-    # translateY y
-myDiagram (TextField _ (Position x y) (Size width height) _) = 
-    square 1 
-    # scaleX width
-    # scaleY height
-    # lwG 0.05
-    # translateX x
-    # translateY y
-myDiagram (ImageField _ (Position x y) (Size width height)) = 
-    square 1
-    #fc white
-    # scaleX width
-    # scaleY height
-    # lwG 0.05
-    # translateX x
-    # translateY y
-myDiagram (Image (Position x y) (Size width height) _) =
-    square 1
-    #fc white
-    # scaleX width
-    # scaleY height
-    # lwG 0.05
-    # translateX x
-    # translateY y
-myDiagram (Text (Position x y) (Size width height) value fontsize) =
-    text value
-    # fontSize (local fontsize) `atop`
-    square 1 
-    #fc white
-    # scaleX width
-    # scaleY height
-    # lwG 0.05
-    # translateX x
-    # translateY y
-myDiagram x = 
-    square 1 
 
-mainDiagram template =
-    renderSVG "diagram.svg" 
-        (dims2D (VKTemplate.width (VKTemplate.size template)) 
-        (VKTemplate.height (VKTemplate.size template))) 
-        (myDiagram template)
+    myDiagram :: LayoutObject -> Diagram B
+    myDiagram (RectLayout _ (Size width height) (Position x y) children)  =
+        (_myDiagramHelper children)
+        # scaleX width
+        # scaleY height
+        # translateX x
+        # translateY y
+    myDiagram (TextField _ (Position x y) (Size width height) _) =
+        square 1
+        # scaleX width
+        # scaleY height
+        # lwG 0.05
+        # translateX x
+        # translateY y
+    myDiagram (ImageField _ (Position x y) (Size width height)) =
+        square 1
+        #fc white
+        # scaleX width
+        # scaleY height
+        # lwG 0.05
+        # translateX x
+        # translateY y
+    myDiagram (Image (Position x y) (Size width height) value) =
+        (unsafePerformIO $ getImage $ drop 7 value)
+        # scaleX width
+        # scaleY height
+        # translateX x
+        # translateY y
+    myDiagram (Text (Position x y) (Size width height) value fontsize) =
+        text value
+        # fontSize (local fontsize)
+        # translateX x
+        # translateY y
+        # fc black
+    myDiagram x =
+        square 1
+
+    getImage :: String -> IO (Diagram B)
+    getImage path = do
+        im <- loadImageEmb path
+        return $ case im of
+            Right r -> image r
+            Left _ -> square 1
+
+    mainDiagram template =
+        let
+            tempW = VKTemplate.width (VKTemplate.size template)
+            tempH = VKTemplate.height (VKTemplate.size template)
+        in
+        renderRasterific "diagram.png"
+            (dims2D tempW tempH)
+            (square 1 # scaleX tempW # scaleY tempH # fc white `atop` myDiagram template)
